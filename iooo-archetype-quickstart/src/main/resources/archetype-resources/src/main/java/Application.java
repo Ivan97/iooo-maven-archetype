aa35138f10893a1baa9c414ca2e7dd0a6b121be5
YangkiZhang <yangkizhang@gmail.com>
2018-05-09 14:04:05 +0800

    [FIX]:[去掉默认vertx]

--- iooo-archetype-quickstart/src/main/resources/archetype-resources/src/main/java/Application.java
+++ iooo-archetype-quickstart/src/main/resources/archetype-resources/src/main/java/Application.java
@@ -3,7 +3,6 @@
 #set( $symbol_escape = '\' )
 package ${package};
 
-import io.vertx.core.Vertx;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -15,7 +14,6 @@ public class Application {
   private static final Logger logger = LoggerFactory.getLogger(Application.class);
 
   public static void main(String[] args) {
-    Vertx vertx = Vertx.vertx();
-    vertx.setPeriodic(10_000, handler -> logger.info("Hello World!"));
+    logger.info("Hello world!");
   }
 }
